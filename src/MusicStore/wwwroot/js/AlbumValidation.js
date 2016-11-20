1a4c70310d6e2cb0090dd5f21ee7f1d4e38f2c78
Dor <benseniord@gmail.com>
2016-11-20 20:08:59 +0200

    validation

---
+++ src/MusicStore/wwwroot/js/AlbumValidation.js
@@ -0,0 +1,10 @@
+﻿function ValidateAlbumForm() {
+    var priceTxt = document.getElementById("Price").value;
+    var price = parseInt(priceTxt);
+    var yearTxt = document.getElementById("Year").value;
+    var year = parseInt(yearTxt);
+    var ok = (price > 0 && year > 0);
+    if (!ok)
+        alert("Invalid fields");
+    return ok;
+}
---
+++ src/MusicStore/wwwroot/js/SongValidation.js
@@ -0,0 +1,8 @@
+﻿function ValidateSongForm() {
+    var orderTxt = document.getElementById("Order").value;
+    var order = parseInt(orderTxt);
+    var ok = (order > 0);
+    if (!ok)
+        alert("Invalid fields");
+    return ok;
+}
